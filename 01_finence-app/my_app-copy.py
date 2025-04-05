5d5f5ac8d9b5a8499ce0caf34e4b82dd661deb32
koromo <shusvlog@gmail.com>
2025-04-05 19:34:27 +0900

    コピーを作成し、コピー元から必要部分の抜き出し

---
+++ 01_finence-app/my_app-copy.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import yfinance as yf
+
+tickers = {
+    'apple': 'AAPL',
+    'meta': 'META',
+    'google': 'GOOGL',
+    'microsoft': 'MSFT',
+    'netflix': 'NFLX',
+    'amazon': 'AMZN'
+}
+
+def get_data(tickers):
+    df = pd.DataFrame()
+    for company in tickers.keys():
+        tkr = yf.Ticker(tickers[company])
+        hist = tkr.history(period=f'{days}d')
+        hist.index = hist.index.strftime('%d %B %Y')
+        hist = hist[['Close']]
+        hist.columns = [company]
+        hist = hist.T
+        hist.index.name = 'Name'
+        df = pd.concat([df, hist])
+    return df
+
+days = 20
+get_data(days, tickers)
