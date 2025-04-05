8705339cddd86eb55b1c6319c62829d792c5f777
koromo <shusvlog@gmail.com>
2025-04-05 16:09:21 +0900

    株価アプリ講義19まで終了

--- 01_finence-app/my_app.py
+++ 01_finence-app/my_app.py
@@ -6,3 +6,19 @@ aapl = yf.Ticker("AAPL")
 days = 20
 hist = aapl.history(period=f'{days}d')
 hist.reset_index()
+
+hist_msft = yf.Ticker("MSFT").history(period=f'{days}d')
+hist_msft.head()
+
+pd.concat([hist, hist_msft], axis=1).head()
+
+hist.index = hist.index.strftime('%d %B %Y')
+hist = hist[['Close']]
+hist.columns = ['apple']
+hist.head(3)
+
+hist = hist.T
+hist
+
+hist.index.name = 'Name'
+hist
