ba5e3073c59b921191bf05a8f8bb320b0f43f68c
koromo <shusvlog@gmail.com>
2025-04-05 20:17:43 +0900

    4-22まで講義終了

--- 01_finence-app/my_app-copy.py
+++ 01_finence-app/my_app-copy.py
@@ -1,6 +1,21 @@
 import pandas as pd
 import yfinance as yf
+import altair as alt
 
+def get_data(days, tickers):
+    df = pd.DataFrame()
+    for company in tickers.keys():
+        tkr = yf.Ticker(tickers[company])
+        hist = tkr.history(period=f'{days}d')
+        hist.index = hist.index.strftime('%d %B %Y')
+        hist = hist[['Close']]
+        hist.columns = [company]
+        hist = hist.T
+        hist.index.name = '企業名'
+        df = pd.concat([df, hist])
+    return df
+
+days = 20
 tickers = {
     'apple': 'AAPL',
     'meta': 'META',
@@ -10,18 +25,15 @@ tickers = {
     'amazon': 'AMZN'
 }
 
-def get_data(tickers):
-    df = pd.DataFrame()
-    for company in tickers.keys():
-        tkr = yf.Ticker(tickers[company])
-        hist = tkr.history(period=f'{days}d')
-        hist.index = hist.index.strftime('%d %B %Y')
-        hist = hist[['Close']]
-        hist.columns = [company]
-        hist = hist.T
-        hist.index.name = 'Name'
-        df = pd.concat([df, hist])
-    return df
+df = get_data(days, tickers)
+df
 
-days = 20
-get_data(days, tickers)
+companies = ['apple', 'meta']
+data = df.loc[companies]
+data.sort_index()
+
+data = data.T.reset_index()
+data = pd.melt(data, id_vars=['Date']).rename(
+    columns = {'value': '株価(USD$)'}
+)
+data
