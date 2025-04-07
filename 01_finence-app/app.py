34c2a3a372b4677013dde797bf4f7c50b2d09c5a
koromo <shusvlog@gmail.com>
2025-04-07 11:23:59 +0900

    講習部分の株価アプリ作成完了

--- 01_finence-app/app.py
+++ 01_finence-app/app.py
@@ -33,46 +33,51 @@ def get_data(days, tickers):
         df = pd.concat([df, hist])
     return df
 
-st.sidebar.write("""
-## 株価の範囲指定
-""")
-ymin, ymax = st.sidebar.slider(
-    '範囲を指定してください。',
-    0.0, 3500.0, (0.0, 3500.0)
-)
-
-tickers = {
-    'apple': 'AAPL',
-    'meta': 'META',
-    'google': 'GOOGL',
-    'microsoft': 'MSFT',
-    'netflix': 'NFLX',
-    'amazon': 'AMZN'
-}
-
-df = get_data(days, tickers)
-companies = st.multiselect(
-    '会社名を選択してください。',
-    list(df.index),
-    ['google', 'amazon', 'meta', 'apple']
-)
-
-if not companies:
-    st.error('少なくとも1社は選んでください。')
-else:
-    data = df.loc[companies]
-    st.write("""### 株価($USD)""", data.sort_index())
-    data = data.T.reset_index()
-    data = pd.melt(data, id_vars=['Date']).rename(
-        columns={'value': '株価($USD)'}
+try:
+    st.sidebar.write("""
+    ## 株価の範囲指定
+    """)
+    ymin, ymax = st.sidebar.slider(
+        '範囲を指定してください。',
+        0.0, 3500.0, (0.0, 3500.0)
     )
-    chart = (
-        alt.Chart(data)
-        .mark_line(opacity=0.8, clip=True)
-        .encode(
-            x="Date:T",
-            y=alt.Y("株価($USD):Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
-            color='企業名:N'
+
+    tickers = {
+        'apple': 'AAPL',
+        'meta': 'META',
+        'google': 'GOOGL',
+        'microsoft': 'MSFT',
+        'netflix': 'NFLX',
+        'amazon': 'AMZN'
+    }
+
+    df = get_data(days, tickers)
+    companies = st.multiselect(
+        '会社名を選択してください。',
+        list(df.index),
+        ['google', 'amazon', 'meta', 'apple']
+    )
+
+    if not companies:
+        st.error('少なくとも1社は選んでください。')
+    else:
+        data = df.loc[companies]
+        st.write("""### 株価($USD)""", data.sort_index())
+        data = data.T.reset_index()
+        data = pd.melt(data, id_vars=['Date']).rename(
+            columns={'value': '株価($USD)'}
         )
+        chart = (
+            alt.Chart(data)
+            .mark_line(opacity=0.8, clip=True)
+            .encode(
+                x="Date:T",
+                y=alt.Y("株価($USD):Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
+                color='企業名:N'
+            )
+        )
+        st.altair_chart(chart, use_container_width=True)
+except:
+    st.error(
+        "何かエラーが起きているようです。"
     )
-    st.altair_chart(chart, use_container_width=True)
